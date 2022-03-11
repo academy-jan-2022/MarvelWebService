f2a343864f97f64b975539ec63b070569a49b7ba
oawood93 <oa.wood@outlook.com>
2022-03-11 13:52:04 +0000

    Initial Commit

---
+++ src/main/java/com/example/codurance/Marvel/MarvelWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.codurance.Marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelWebApplication.class, args);
+	}
+
+}
